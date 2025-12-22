a5da83f7fa0dc2b01de463ae23d7f0e5df93e416
Rodrigo Wienandts Ferreira <rowife0909@gmail.com>
2025-12-22 10:11:01 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { SearchIcon } from "lucide-react";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "@/@app/_components/ui/badge";
+import { Avatar, AvatarImage } from "@/@app/_components/ui/avatar";
 
 const app = () => {
   return (
@@ -25,6 +28,25 @@ const app = () => {
             className="object-cover rounded-xl"
           />
         </div>
+        <Card className="mt-6">
+          <CardContent className="flex justify-between">
+            <div className="flex flex-col gap-2 py-5">
+              <Badge>Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Dezembro</p>
+              <p className="text-2xl">22</p>
+              <p className="text-sm">12:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
